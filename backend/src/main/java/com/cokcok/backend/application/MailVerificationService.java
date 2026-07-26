71e0903546f9720158390219e94895c142676bf8
신영한 <cokcok.support@gmail.com>
2026-07-26 18:24:13 +0900

    feat: mail verification code save and confirm feature 계층 구성 #17

--- backend/src/main/java/com/cokcok/backend/application/MailVerificationService.java
+++ backend/src/main/java/com/cokcok/backend/application/MailVerificationService.java
@@ -1,19 +1,24 @@
 package com.cokcok.backend.application;
 
 import com.cokcok.backend.adapter.utils.GeneratorUtils;
+import com.cokcok.backend.application.request.MailVerificationCodeConfirmServiceRequest;
 import com.cokcok.backend.application.request.MailVerificationCodeServiceRequest;
+import com.cokcok.backend.application.required.MailVerificationRepository;
+import com.cokcok.backend.domain.MailVerification;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
-public class MailService {
+@Transactional(readOnly = true)
+public class MailVerificationService {
     private static final String MAIL_MESSAGE_ENCODING = "UTF-8";
     private static final String MAIL_MESSAGE_SUBJECT = "[콕콕] 회원 가입을 위한 인증 코드가 도착했습니다.";
     private static final int MAIL_MESSAGE_CODE_LENGTH = 6;
@@ -22,11 +27,23 @@ public class MailService {
 
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
+    private final MailVerificationRepository mailVerificationRepository;
 
+    public MailVerification confirmVerificationCode(MailVerificationCodeConfirmServiceRequest request) {
+        MailVerification mailVerification = mailVerificationRepository.findByEmail(request.getEmail());
+        mailVerification.verify(request.getCode());
+
+        return mailVerification;
+    }
+
+    @Transactional
     public void sendVerificationCode(MailVerificationCodeServiceRequest request) {
-
-        String verificationCode = GeneratorUtils.generateCode(MAIL_MESSAGE_CODE_LENGTH, 0, 10);
+        String verificationCode = GeneratorUtils.generateRandomCode(MAIL_MESSAGE_CODE_LENGTH, 0, 10);
         String SignUpVerificationCodeMailHtml = processVerificationCodeMailHtml(verificationCode);
+
+        MailVerification mailVerification = MailVerification.create(request.getEmail(), verificationCode);
+        mailVerificationRepository.save(mailVerification);
+        System.out.println(verificationCode);
 
         try {
             MimeMessage message = mailSender.createMimeMessage();
@@ -35,6 +52,7 @@ public class MailService {
             helper.setSubject(MAIL_MESSAGE_SUBJECT);
             helper.setText(SignUpVerificationCodeMailHtml, true);
             mailSender.send(message);
+
         } catch (MessagingException exception) {
             System.out.println("실패 처리 코드 추가 예정");
         }
